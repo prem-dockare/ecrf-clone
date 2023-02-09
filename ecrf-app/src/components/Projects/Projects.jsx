b73bf9b7e9f361b1178629ab497ae0360add7838
Premkumar Hulikoppe <premkumar@dockare.io>
2023-02-09 16:21:18 +0530

    added create new button at projects page

--- ecrf-app/src/components/Projects/Projects.jsx
+++ ecrf-app/src/components/Projects/Projects.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import TabsPanel from "./components/Tabs/Tabs";
+import CreateNewBtn from "./components/CreateNewBtn/CreateNewBtn";
 const Projects = () => {
+  const containerStyle = {
+    position: "absolute",
+    top: "125px",
+    left: "45px",
+    right: "45px",
+    // border: "1px solid black",
+  };
   return (
     <>
       <NavBar />
-      <TabsPanel />
+      <div style={containerStyle}>
+        <TabsPanel />
+        <CreateNewBtn />
+      </div>
     </>
   );
 };
---
+++ ecrf-app/src/components/Projects/components/CreateNewBtn/CreateNewBtn.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Box } from "@chakra-ui/react";
+import { AddIcon } from "@chakra-ui/icons";
+
+const CreateNewBtn = () => {
+  const container = {
+    display: "flex",
+    justifyContent: "flex-end",
+    width: "100%",
+  };
+  const btn = {
+    width: "161px",
+    height: "40px",
+    background: "rgba(79, 79, 79, 0.4)",
+    borderRadius: "10px",
+    fontWeight: "500",
+    fontSize: "20px",
+    lineHeight: "24px",
+    padding: "8px 16px",
+    color: "#F8F8F8",
+  };
+  return (
+    <Box sx={container}>
+      <Button sx={btn} colorScheme="teal" variant="solid">
+        + Create new
+      </Button>
+    </Box>
+  );
+};
+
+export default CreateNewBtn;
